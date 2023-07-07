f490040ba140b30ce4f0608594c30a75843eb38d
Sherman Chen <schen@simviu.com>
2023-07-07 22:17:23 +0800

    [uwb]: OLED display ok with ss_oled lib.

--- test/uwb/uwbRange/DW1000Ranging_ANCHOR1/DW1000Ranging_ANCHOR1.ino
+++ test/uwb/uwbRange/DW1000Ranging_ANCHOR1/DW1000Ranging_ANCHOR1.ino
@@ -6,11 +6,8 @@
  *  - give example description
  */
 #include <SPI.h>
-
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SH110X.h>
-
+#include <ss_oled.h>
 #include "DW1000Ranging.h"
 
 //---- connection pins (Xiao rp2040)
@@ -23,44 +20,79 @@ const uint8_t PIN_RST = 9; // reset pin
 const uint8_t PIN_IRQ = 2; // irq pin
 const uint8_t PIN_SS = SS; // spi select pin
 
-//---- display
+//---- display-------------
 
-/* Uncomment the initialize the I2C address , uncomment only one, If you get a totally blank screen try the other*/
-#define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
-//#define i2c_Address 0x3d //initialize with the I2C addr 0x3D Typically Adafruit OLED's
+// if your system doesn't have enough RAM for a back buffer, comment out
+// this line (e.g. ATtiny85)
+//#define USE_BACKBUFFER
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET -1   //   QT-PY / XIAO
+#ifdef USE_BACKBUFFER
+static uint8_t ucBackBuffer[1024];
+#else
+static uint8_t *ucBackBuffer = NULL;
+#endif
+
+// Use -1 for the Wire library default pins
+// or specify the pin numbers to use with the Wire library or bit banging on any GPIO pins
+// These are the pin numbers for the M5Stack Atom default I2C
+#define SDA_PIN 18 //21
+#define SCL_PIN 19 //22
+// Set this to -1 to disable or the GPIO pin number connected to the reset
+// line of your display if it requires an external reset
+#define RESET_PIN -1
+// let ss_oled figure out the display address
+#define OLED_ADDR -1
+// don't rotate the display
+#define FLIP180 0
+// don't invert the display
+#define INVERT 0
+// Bit-Bang the I2C bus
+#define USE_HW_I2C 0
+
+// Change these if you're using a different OLED display
+#define MY_OLED OLED_128x64
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 64
+//#define MY_OLED OLED_64x32
+//#define OLED_WIDTH 64
+//#define OLED_HEIGHT 32
+
+SSOLED ssoled;
 
 
-//Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+//----------------------
 
-
-/*
 //-----
 void init_display()
 {
-    display.begin(i2c_Address, true); // Address 0x3C default
-    //display.setContrast (0); // dim display
-  
-    display.display();
+    
+      int rc;
+  // The I2C SDA/SCL pins set to -1 means to use the default Wire library
+  // If pins were specified, they would be bit-banged in software
+  // This isn't inferior to hw I2C and in fact allows you to go faster on certain CPUs
+  // The reset pin is optional and I've only seen it needed on larger OLEDs (2.4")
+  //    that can be configured as either SPI or I2C
+  //
+  // oledInit(SSOLED *, type, oled_addr, rotate180, invert, bWire, SDA_PIN, SCL_PIN, RESET_PIN, speed)
+
+  rc = oledInit(&ssoled, MY_OLED, OLED_ADDR, FLIP180, INVERT, USE_HW_I2C, SDA_PIN, SCL_PIN, RESET_PIN, 400000L); // use standard I2C bus at 400Khz
+    if (rc == OLED_NOT_FOUND)
+    {
+        Serial.print("OLED not found");
+        return;
+    }
+    
+    //----
+    char *msgs[] = {(char *)"SSD1306 @ 0x3C", (char *)"SSD1306 @ 0x3D",(char *)"SH1106 @ 0x3C",(char *)"SH1106 @ 0x3D"};
+    oledFill(&ssoled, 0, 1);
+    oledWriteString(&ssoled, 0,0,0,msgs[rc], FONT_NORMAL, 0, 1);
+    oledSetBackBuffer(&ssoled, ucBackBuffer);
     delay(2000);
-
-    // Clear the buffer.
-    display.clearDisplay();  
-
-    // text display tests
-  display.setTextSize(2);
-  display.setTextColor(SH110X_WHITE);
-  display.setCursor(0, 0);
-
-  //----
-  display.println("UWB anchor");
+    
 
 }
 
-*/
+
 //-----
 void setup() {
   Serial.begin(115200);
@@ -112,7 +144,7 @@ void setup() {
     Serial.print("Device mode: "); Serial.println(msg);
   }
   //---- display info
-  /*
+  
   if(true)
   {
     init_display();
@@ -122,10 +154,17 @@ void setup() {
     Serial.print("Network ID & Device Address: "); 
     Serial.println(msg);
     
-    Serial.print("NetID & DevAddr"); 
-    display.println(msg);
+    oledFill(&ssoled, 0x0, 1);
+   // oledWriteString(&ssoled, 0,16,0,(char *)"Hello UWB", FONT_NORMAL, 0, 1);
+   // oledWriteString(&ssoled, 0,0,1,(char *)"NetID & DevAddr", FONT_SMALL, 1, 1);
+    oledWriteString(&ssoled, 0,0,2,(char *)">", FONT_NORMAL, 0, 1);
+
+    oledSetTextWrap(&ssoled, true);
+    oledWriteString(&ssoled, 0,-1,-1,(char *)msg, FONT_SMALL, 1, 1);
+//    oledWriteString(&ssoled, 0,0,3,(char*)"FL", FONT_LARGE, 0, 1);
+
   }
-  */
+  
 }
 
 void loop() {
